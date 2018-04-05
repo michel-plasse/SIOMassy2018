50059c404cf74c941d7cce910cd5f9502f862b2b
Mohamed dennoun <Mohamedennoun@gmail.com>
2018-04-05 18:08:11 +0200

    ajout de la classe evaluation et servlet et jsp et liste de formation

---
+++ src/java/controller/EvaluationServlet.java
@@ -0,0 +1,37 @@
+package controller;
+
+import dao.EvaluationDao;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Evaluation;
+
+/**
+ * Servlet implementation class EvaluationServlet
+ */
+@WebServlet(name = "EvaluationServlet", urlPatterns = {"/evaluation"})
+public class EvaluationServlet extends HttpServlet {
+    private final String VUE_OK = "/WEB-INF/evaluations.jsp";
+    private final String VUE_ERREUR = "/WEB-INF/message.jsp";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String vue = VUE_OK;
+        try {
+            EvaluationDao dao = new EvaluationDao();
+            List<Evaluation> evaluations = dao.getOuvertes();
+            request.setAttribute("evaluation", evaluations);
+            
+        } catch (SQLException exc) {
+            exc.printStackTrace();
+            request.setAttribute("message", "Probleme  de bases de données ");
+            vue = VUE_ERREUR;
+        }
+        getServletContext().getRequestDispatcher(vue).forward(request, response);
+    }
+}
